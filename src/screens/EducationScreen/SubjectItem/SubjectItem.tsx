23ad1f1c82f3e23de89d11a7d0de177a1fa51a32
tklongg <klong31122001@gmail.com>
2024-04-17 19:15:07 +0700

    add

--- src/screens/EducationScreen/SubjectItem/SubjectItem.tsx
+++ src/screens/EducationScreen/SubjectItem/SubjectItem.tsx
@@ -7,10 +7,14 @@ import SubjectSheet from '../SubjectSheet/SubjectSheet'
 
 const SubjectItem = ({ isGraded, title }: { isGraded: boolean, title: string }) => {
     const [isEditing, setIsEditing] = React.useState(false)
-    const [isGradded, setIsGradded] = React.useState(false)
+    const [isGradded, setIsGradded] = React.useState(isGraded)
     const [expected, setExpected] = React.useState(100)
     const [score, setScore] = React.useState(0)
     const bottomSheetRef = React.useRef<RBSheet>(null)
+    React.useEffect(() => {
+        if (score !== 0) setIsGradded(true)
+        else setIsGradded(false)
+    }, [score])
     return (
         <TouchableOpacity className='flex-row items-center  border-b-[1px] border-gray-200 bg-white' onPress={() => {
             bottomSheetRef.current?.open()
@@ -21,7 +25,7 @@ const SubjectItem = ({ isGraded, title }: { isGraded: boolean, title: string }) 
             <View className=' w-36 items-center border-l-[1px] border-gray-200 py-6 px-5 bg-[#f8f7fd]'>
 
                 {
-                    isGraded ?
+                    isGradded ?
                         <><Text className='  text-base opacity-40' style={{ color: COLORS.primary }}>score</Text><Text className=' text-[#918D92] text-xl mt-1 opacity-40'>{score} / 100</Text></> :
 
                         <><Text className='  text-base' style={{ color: COLORS.primary }}>need</Text><Text className='  text-xl mt-1'>{expected}</Text></>
