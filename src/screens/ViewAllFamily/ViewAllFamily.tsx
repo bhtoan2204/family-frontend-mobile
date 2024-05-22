64d4dd82fc56bfa22782a17873bcd5ae58b6f89b
dthgiang <duongthanhgiang.0108@gmail.com>
2024-05-22 18:23:00 +0700

    Draw background for ViewAllFamily Screen

--- src/screens/ViewAllFamily/ViewAllFamily.tsx
+++ src/screens/ViewAllFamily/ViewAllFamily.tsx
@@ -7,6 +7,7 @@ import {
   TextInput,
   Image,
   Modal,
+  ImageBackground,
 } from 'react-native';
 import {FamilyServices} from 'src/services/apiclient';
 import {Feather as FeatherIcon, MaterialIcons} from '@expo/vector-icons';
@@ -55,7 +56,7 @@ const ViewAllFamilyScreen: React.FC<ViewAllFamilyScreenProps> = ({
       {selectedButton === title ? (
         <LinearGradient
           colors={
-            isUp ? ['#537895', '#09203F'] : ['#c7d5e0', '#c7d5e0', '#c7d5e0']
+            isUp ? ['#537895', '#09203F'] : ['#CCEAFB', '#CCEAFB', '#CCEAFB']
           }
           style={[styles.button1, buttonStyle]}
           start={{x: 0, y: 0}}
@@ -64,7 +65,7 @@ const ViewAllFamilyScreen: React.FC<ViewAllFamilyScreenProps> = ({
             <Text
               style={[
                 styles.buttonTextChoosen,
-                {color: isUp ? '#fff' : '#1E3B70'},
+                {color: isUp ? '#fff' : '#2a475e'},
                 {fontSize: 16},
               ]}>
               {title}
@@ -72,23 +73,23 @@ const ViewAllFamilyScreen: React.FC<ViewAllFamilyScreenProps> = ({
             <MaterialIcons
               name={isUp ? 'keyboard-arrow-up' : 'keyboard-arrow-down'}
               size={25}
-              color={isUp ? '#fff' : '#1E3B70'}
+              color={isUp ? '#fff' : '#2a475e'}
               style={styles.iconWrapper}
             />
           </View>
         </LinearGradient>
       ) : (
         <View
-          style={[styles.button1, buttonStyle, {backgroundColor: '#c7d5e0'}]}>
+          style={[styles.button1, buttonStyle, {backgroundColor: '#CCEAFB'}]}>
           <View style={[styles.row, {alignItems: 'center'}]}>
             <Text
-              style={[styles.buttonText, {color: '#1E3B70'}, {fontSize: 16}]}>
+              style={[styles.buttonText, {color: '#2a475e'}, {fontSize: 16}]}>
               {title}
             </Text>
             <MaterialIcons
               name="keyboard-arrow-down"
               size={25}
-              color="#1E3B70"
+              color="#2a475e"
               style={styles.iconWrapper}
             />
           </View>
@@ -122,210 +123,217 @@ const ViewAllFamilyScreen: React.FC<ViewAllFamilyScreenProps> = ({
   }, [navigation]);
 
   return (
-    <View style={[{flex: 1, backgroundColor: '#fff'}]}>
-      <View style={styles.circleContainer}>
-        <TouchableOpacity style={styles.circle}>
-          <MaterialIcons
-            name="keyboard-arrow-left"
-            size={32}
-            //color="#56409e"
-            color="#2a475e"
+    <ImageBackground
+      source={require('../../assets/images/view-all-family.png')}
+      style={{flex: 1}}
+      resizeMode="stretch">
+      <View style={[{flex: 1}]}>
+        <View style={styles.circleContainer}>
+          <TouchableOpacity style={styles.circle}>
+            <MaterialIcons
+              name="keyboard-arrow-left"
+              size={32}
+              //color="#56409e"
+              color="#2a475e"
+            />
+          </TouchableOpacity>
+          <Text style={styles.headerTitle1}>Family&Member</Text>
+          <TouchableOpacity style={styles.circle}>
+            <Material
+              name="home-plus-outline"
+              size={25}
+              //color="#56409e"
+              color="#2a475e"
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={[styles.inputWrapper, {left: 20, marginBottom: 15}]}>
+          <TextInput
+            placeholder="Search Families"
+            placeholderTextColor="#9C9AAF"
+            style={styles.input}
+            onChangeText={text => setSearchTerm(text)}
+            value={searchTerm}
           />
-        </TouchableOpacity>
-        <Text style={styles.headerTitle1}>Family&Member</Text>
-        <TouchableOpacity style={styles.circle}>
-          <Material
-            name="home-plus-outline"
-            size={25}
-            //color="#56409e"
-            color="#2a475e"
+          <TouchableOpacity onPress={handleSearch}>
+            <Material
+              name="home-search-outline"
+              size={25}
+              //color="#56409e"
+              color="#2a475e"
+              style={styles.inputIcon}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={[styles.row, {padding: 5}, {margin: 3}]}>
+          <Button title="Name" buttonStyle={{left: 10}} iconName={''} />
+          <Button
+            title="Date"
+            buttonStyle={{}} // Add an empty object or specify the desired style
+            iconName={''}
           />
-        </TouchableOpacity>
-      </View>
+          <Button title="Recently" buttonStyle={{right: 10}} iconName={''} />
+        </View>
 
-      <View style={[styles.inputWrapper, {left: 20, marginBottom: 15}]}>
-        <TextInput
-          placeholder="Search Families"
-          placeholderTextColor="#9C9AAF"
-          style={styles.input}
-          onChangeText={text => setSearchTerm(text)}
-          value={searchTerm}
-        />
-        <TouchableOpacity onPress={handleSearch}>
-          <Material
-            name="home-search-outline"
-            size={25}
-            //color="#56409e"
-            color="#2a475e"
-            style={styles.inputIcon}
-          />
-        </TouchableOpacity>
-      </View>
-
-      <View style={[styles.row, {padding: 5}, {margin: 3}]}>
-        <Button title="Name" buttonStyle={{left: 10}} iconName={''} />
-        <Button
-          title="Date"
-          buttonStyle={{}} // Add an empty object or specify the desired style
-          iconName={''}
-        />
-        <Button title="Recently" buttonStyle={{right: 10}} iconName={''} />
-      </View>
-
-      <Animated.ScrollView
-        contentContainerStyle={styles.content}
-        onScroll={Animated.event(
-          [{nativeEvent: {contentOffset: {y: scrollY}}}],
-          {
-            useNativeDriver: true,
-          },
-        )}
-        scrollEventThrottle={1}>
-        {families.map((family, index) => (
-          <View key={index} style={styles.familyBigCard}>
-            <View style={styles.card}>
-              <TouchableOpacity>
-                <Image
-                  source={require('../../assets/images/far-cry-family.png')}
-                  resizeMode="center"
-                  style={styles.image}
-                />
-              </TouchableOpacity>
-              <View style={[styles.row]}>
-                <View style={styles.cardContainer2}>
-                  <Text style={styles.cardTitle}>{family.name}</Text>
-                  <View style={styles.ColorAndDescription}>
-                    <TouchableOpacity style={styles.color}>
-                      <Material name="alien-outline" size={25} color="#fff" />
-                    </TouchableOpacity>
-                    <Text style={[styles.cardDescription, {marginLeft: 10}]}>
-                      {family.description}
-                    </Text>
-                  </View>
-                  <View style={styles.buttonPos}>
-                    <LinearGradient
-                      colors={['#09203F', '#537895']}
-                      style={[styles.button, styles.detailButton]}
-                      start={{x: 0, y: 1}}
-                      end={{x: 0, y: 0}}>
-                      <TouchableOpacity
-                        style={{
-                          shadowColor: '#000',
-                          shadowOffset: {width: 0, height: 6},
-                          shadowOpacity: 0.25,
-                        }}
-                        onPress={() =>
-                          navigation.navigate('ViewFamily', {
-                            id_family: family.id_family,
-                          })
-                        }>
-                        <Text
-                          style={[
-                            styles.buttonText,
-                            {color: '#fff'},
-                            {fontWeight: '600'},
-                          ]}>
-                          View Detail
-                        </Text>
-                      </TouchableOpacity>
-                    </LinearGradient>
-                  </View>
-                </View>
-
-                <TouchableOpacity
-                  style={{right: 60}}
-                  onPressIn={event => {
-                    const locationX = event.nativeEvent.pageX;
-                    const locationY = event.nativeEvent.pageY;
-                    setButtonPosition({
-                      x: locationX,
-                      y: locationY,
-                    });
-                    setModalVisible(true);
-                  }}>
-                  <MaterialIcons name="more-vert" size={30} color="#1b2838" />
+        <Animated.ScrollView
+          contentContainerStyle={styles.content}
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {y: scrollY}}}],
+            {
+              useNativeDriver: true,
+            },
+          )}
+          scrollEventThrottle={1}>
+          {families.map((family, index) => (
+            <View key={index} style={styles.familyBigCard}>
+              <View style={styles.card}>
+                <TouchableOpacity>
+                  <Image
+                    source={require('../../assets/images/far-cry-family.png')}
+                    resizeMode="center"
+                    style={styles.image}
+                  />
                 </TouchableOpacity>
+                <View style={[styles.row]}>
+                  <View style={styles.cardContainer2}>
+                    <Text style={styles.cardTitle}>{family.name}</Text>
+                    <View style={styles.ColorAndDescription}>
+                      <TouchableOpacity style={styles.color}>
+                        <Material name="alien-outline" size={25} color="#fff" />
+                      </TouchableOpacity>
+                      <Text style={[styles.cardDescription, {marginLeft: 10}]}>
+                        {family.description}
+                      </Text>
+                    </View>
+                    <View style={styles.buttonPos}>
+                      <LinearGradient
+                        colors={['#09203F', '#537895']}
+                        style={[styles.button, styles.detailButton]}
+                        start={{x: 0, y: 1}}
+                        end={{x: 0, y: 0}}>
+                        <TouchableOpacity
+                          style={{
+                            shadowColor: '#000',
+                            shadowOffset: {width: 0, height: 6},
+                            shadowOpacity: 0.25,
+                          }}
+                          onPress={() =>
+                            navigation.navigate('ViewFamily', {
+                              id_family: family.id_family,
+                            })
+                          }>
+                          <Text
+                            style={[
+                              styles.buttonText,
+                              {color: '#fff'},
+                              {fontWeight: '600'},
+                            ]}>
+                            View Detail
+                          </Text>
+                        </TouchableOpacity>
+                      </LinearGradient>
+                    </View>
+                  </View>
 
-                <Modal
-                  animationType="fade"
-                  transparent={true}
-                  visible={modalVisible}
-                  onRequestClose={() => {
-                    setModalVisible(!modalVisible);
-                  }}>
-                  <View style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.5)'}}>
-                    <TouchableOpacity
-                      style={{flex: 1}}
-                      activeOpacity={1}
-                      onPressOut={() => setModalVisible(false)}>
-                      <View
-                        style={{
-                          position: 'absolute',
-                          top: buttonPosition.y + 15, // add 10 to y position
-                          left: buttonPosition.x - 108, // add 10 to x position
-                        }}>
+                  <TouchableOpacity
+                    style={{right: 60}}
+                    onPressIn={event => {
+                      const locationX = event.nativeEvent.pageX;
+                      const locationY = event.nativeEvent.pageY;
+                      setButtonPosition({
+                        x: locationX,
+                        y: locationY,
+                      });
+                      setModalVisible(true);
+                    }}>
+                    <MaterialIcons name="more-vert" size={30} color="#1b2838" />
+                  </TouchableOpacity>
+
+                  <Modal
+                    animationType="fade"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                      setModalVisible(!modalVisible);
+                    }}>
+                    <View style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.5)'}}>
+                      <TouchableOpacity
+                        style={{flex: 1}}
+                        activeOpacity={1}
+                        onPressOut={() => setModalVisible(false)}>
                         <View
                           style={{
-                            backgroundColor: '#f6f6f6',
-                            padding: 15,
-                            borderRadius: 10,
-                            width: 130,
+                            position: 'absolute',
+                            top: buttonPosition.y + 15, // add 10 to y position
+                            left: buttonPosition.x - 108, // add 10 to x position
                           }}>
-                          <TouchableOpacity
-                            onPress={() => {
-                              setModalVisible(false);
-                              alert('Da bam Edit');
+                          <View
+                            style={{
+                              backgroundColor: '#f6f6f6',
+                              padding: 15,
+                              borderRadius: 10,
+                              width: 130,
                             }}>
-                            <View style={[styles.row, {alignItems: 'center'}]}>
-                              <Text
-                                style={[
-                                  {color: '#2a475e'},
-                                  {fontWeight: '700'},
-                                  {fontSize: 16},
-                                ]}>
-                                Edit
-                              </Text>
-                              <Material
-                                name="pencil-outline"
-                                size={30}
-                                color="#2a475e"
-                              />
-                            </View>
-                          </TouchableOpacity>
-                          <TouchableOpacity
-                            onPress={() => {
-                              setModalVisible(false);
-                              alert('Da bam Delete');
-                            }}>
-                            <View style={[styles.row, {alignItems: 'center'}]}>
-                              <Text
-                                // style={[
-                                //   {color: '#724DC9'},
-                                //   {fontWeight: '700'},
-                                //   {fontSize: 16},
-                                // ]}
-                                style={styles.text}>
-                                Delete
-                              </Text>
-                              <MaterialIcons
-                                name="delete-outline"
-                                size={30}
-                                color="#2a475e"
-                                style={styles.icon}
-                              />
-                            </View>
-                          </TouchableOpacity>
+                            <TouchableOpacity
+                              onPress={() => {
+                                setModalVisible(false);
+                                alert('Da bam Edit');
+                              }}>
+                              <View
+                                style={[styles.row, {alignItems: 'center'}]}>
+                                <Text
+                                  style={[
+                                    {color: '#2a475e'},
+                                    {fontWeight: '700'},
+                                    {fontSize: 16},
+                                  ]}>
+                                  Edit
+                                </Text>
+                                <Material
+                                  name="pencil-outline"
+                                  size={30}
+                                  color="#2a475e"
+                                />
+                              </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                              onPress={() => {
+                                setModalVisible(false);
+                                alert('Da bam Delete');
+                              }}>
+                              <View
+                                style={[styles.row, {alignItems: 'center'}]}>
+                                <Text
+                                  // style={[
+                                  //   {color: '#724DC9'},
+                                  //   {fontWeight: '700'},
+                                  //   {fontSize: 16},
+                                  // ]}
+                                  style={styles.text}>
+                                  Delete
+                                </Text>
+                                <MaterialIcons
+                                  name="delete-outline"
+                                  size={30}
+                                  color="#2a475e"
+                                  style={styles.icon}
+                                />
+                              </View>
+                            </TouchableOpacity>
+                          </View>
                         </View>
-                      </View>
-                    </TouchableOpacity>
-                  </View>
-                </Modal>
+                      </TouchableOpacity>
+                    </View>
+                  </Modal>
+                </View>
               </View>
             </View>
-          </View>
-        ))}
-      </Animated.ScrollView>
-    </View>
+          ))}
+        </Animated.ScrollView>
+      </View>
+    </ImageBackground>
   );
 };
 export default ViewAllFamilyScreen;
