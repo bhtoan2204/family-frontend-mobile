d74f82578435685f99b2576cd5d39d78f9a13d5c
dthgiang <duongthanhgiang.0108@gmail.com>
2024-05-28 15:54:43 +0700

    chore: Refactor navigation container component SafeAreaView

--- src/navigation/NavigationContainer.tsx
+++ src/navigation/NavigationContainer.tsx
@@ -1,8 +1,5 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { COLORS } from 'src/constants';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AuthStack from './Routes/AuthStack';
 import HomeTab from './Routes/HomeTab';
 import FamilyStack from './Routes/FamilyStack';
@@ -16,31 +13,21 @@ const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
-    <SafeAreaView style={styles.safeAreaStyle}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="AuthStack" component={AuthStack} />
-          <Stack.Screen name="HomeTab" component={HomeTab} />
-          <Stack.Screen name="FamilyStack" component={FamilyStack} />
-          <Stack.Screen name="PackStack" component={PackStack} />
-          <Stack.Screen name="CalendarStack" component={CalendarStack} />
-          <Stack.Screen name="ChatStack" component={ChatStack} />
-          <Stack.Screen name="ExpenseStack" component={ExpenseStack} />
-          <Stack.Screen name="IncomeStack" component={IncomeStack} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="AuthStack" component={AuthStack} />
+        <Stack.Screen name="HomeTab" component={HomeTab} />
+        <Stack.Screen name="FamilyStack" component={FamilyStack} />
+        <Stack.Screen name="PackStack" component={PackStack} />
+        <Stack.Screen name="CalendarStack" component={CalendarStack} />
+        <Stack.Screen name="ChatStack" component={ChatStack} />
+        <Stack.Screen name="ExpenseStack" component={ExpenseStack} />
+        <Stack.Screen name="IncomeStack" component={IncomeStack} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  safeAreaStyle: {
-    flex: 1,
-  },
-});
-
 export default Navigation;
