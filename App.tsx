50071219215b42b8e9983d4a3f69e04ba6d8412a
dthgiang <duongthanhgiang.0108@gmail.com>
2024-06-29 02:54:52 +0700

    Fix bug

--- App.tsx
+++ App.tsx
@@ -1,35 +1,32 @@
-import React, { useEffect } from 'react';
-import { Provider } from 'react-redux';
-import { PaperProvider } from 'react-native-paper';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React, {useEffect} from 'react';
+import {Provider} from 'react-redux';
+import {PaperProvider} from 'react-native-paper';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import NavigationContainer from 'src/navigation';
-import { store } from 'src/redux/store';
+import {store} from 'src/redux/store';
 import Notification from 'src/screens/Notifications';
-import { connectSocket } from 'src/services/apiclient/Socket';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { ActionSheetProvider } from '@expo/react-native-action-sheet';
-import { MenuProvider } from 'react-native-popup-menu';
-import {
-  BottomSheetModal,
-  BottomSheetView,
-  BottomSheetModalProvider,
-} from '@gorhom/bottom-sheet';
+import {connectSocket} from 'src/services/apiclient/Socket';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {ActionSheetProvider} from '@expo/react-native-action-sheet';
+import {MenuProvider} from 'react-native-popup-menu';
+// import {
+//   BottomSheetModal,
+//   BottomSheetView,
+//   BottomSheetModalProvider,
+// } from '@gorhom/bottom-sheet';
 const App: React.FC = () => {
   useEffect(() => {
     connectSocket();
   }, []);
-  
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
-        <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{flex: 1}}>
           <PaperProvider>
             <ActionSheetProvider>
               <MenuProvider>
-                <BottomSheetModalProvider>
-
-                  <NavigationContainer />
-                </BottomSheetModalProvider>
+                <NavigationContainer />
               </MenuProvider>
             </ActionSheetProvider>
           </PaperProvider>
