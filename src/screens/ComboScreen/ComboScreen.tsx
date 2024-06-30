9d0ebfe0abe4a9c733572226a53dc294ce582a30
dthgiang <duongthanhgiang.0108@gmail.com>
2024-06-30 07:25:33 +0700

    Draw Analysis Finance scroll screen

--- src/screens/ComboScreen/ComboScreen.tsx
+++ src/screens/ComboScreen/ComboScreen.tsx
@@ -7,12 +7,80 @@ import {COLORS, TEXTS} from 'src/constants';
 
 import {PackageServices} from 'src/services/apiclient';
 import styles from './styles';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const ComboScreen = () => {
   return (
-    <View>
-      <Text style={styles.title}>ComboScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.analysis}>
+          <View style={{bottom: 10}}>
+            <Image
+              source={require('../../assets/images/combo-package/Analysis_Finance/Information.png')}
+              resizeMode="stretch"
+              style={styles.imageInformation}
+            />
+            <Image
+              source={require('../../assets/images/combo-package/Analysis_Finance/red-gradient.png')}
+              resizeMode="stretch"
+              style={styles.redGradient}
+            />
+            <Image
+              source={require('../../assets/images/combo-package/Analysis_Finance/Character.png')}
+              resizeMode="stretch"
+              style={styles.imageCharacter}
+            />
+            <Image
+              source={require('../../assets/images/combo-package/Analysis_Finance/linechart.png')}
+              resizeMode="stretch"
+              style={styles.imageLineChart}
+            />
+            <Image
+              source={require('../../assets/images/combo-package/Analysis_Finance/piechart.png')}
+              resizeMode="stretch"
+              style={styles.imagePieChart}
+            />
+            <Image
+              source={require('../../assets/images/combo-package/Analysis_Finance/barchart.png')}
+              resizeMode="stretch"
+              style={styles.imageBarChart}
+            />
+            <Image
+              source={require('../../assets/images/combo-package/Analysis_Finance/back.png')}
+              resizeMode="stretch"
+              style={styles.imageBack}
+            />
+            <Image
+              source={require('../../assets/images/combo-package/Analysis_Finance/front.png')}
+              resizeMode="stretch"
+              style={styles.imageFront}
+            />
+          </View>
+          <View style={styles.bottomField}>
+            <Image
+              source={require('../../assets/images/combo-package/Analysis_Finance/background-bottom.png')}
+              resizeMode="stretch"
+              style={styles.imageBottom}
+            />
+            <Text style={styles.title}>Analysis Finance</Text>
+            <Text style={styles.detail}>
+              Track finances, analyze spending, plan budgets and visualize money
+              flow with loved ones.
+            </Text>
+          </View>
+          <Image
+            source={require('../../assets/images/combo-package/Analysis_Finance/wave-bottom.png')}
+            resizeMode="stretch"
+            style={styles.imageBottomWave}
+          />
+          <Image
+            source={require('../../assets/images/combo-package/Analysis_Finance/background-top.png')}
+            resizeMode="stretch"
+            style={styles.imageTop}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
