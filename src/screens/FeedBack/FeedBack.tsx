541cbc338d3d2e24c9b10fe7c9a644261841cc3f
dthgiang <duongthanhgiang.0108@gmail.com>
2024-07-27 00:59:01 +0700

    Update Feedback UI

--- src/screens/FeedBack/FeedBack.tsx
+++ src/screens/FeedBack/FeedBack.tsx
@@ -8,6 +8,7 @@ import {
   KeyboardAvoidingView,
   Platform,
   ScrollView,
+  Image,
 } from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 import {useNavigation} from '@react-navigation/native';
@@ -56,44 +57,68 @@ const FeedbackScreen = () => {
       <TouchableOpacity
         style={styles.backButton}
         onPress={() => navigation.navigate('HomeTab', {screen: 'HomeScreen'})}>
-        <Ionicons name="close" size={32} color={color.text} />
+        <Ionicons
+          name="close"
+          size={32}
+          color={color.text}
+          style={{marginLeft: 10, marginTop: 20}}
+        />
       </TouchableOpacity>
       <ScrollView contentContainerStyle={styles.scrollViewContainer}>
-        <Text style={[styles.title, {color: color.text}]}>
-          {translate('YourFeedback')}
-        </Text>
-        <Text style={[styles.subtitle, {color: color.text}]}>
-          {translate('FeedbackQuestion')}
-        </Text>
-        <View style={styles.ratingContainer}>
-          {Array.from({length: 5}, (_, index) => (
+        <View style={{alignItems: 'center', bottom: 50}}>
+          <Image
+            source={require('../../assets/images/feedback-star.png')}
+            resizeMode="stretch"
+            style={{width: 250, height: 250, top: 100, zIndex: 1}}
+          />
+          <View
+            style={{
+              backgroundColor: color.card,
+              width: 350,
+              padding: 30,
+              alignItems: 'center',
+              borderRadius: 20,
+            }}>
+            <View style={{backgroundColor: 'transparent', height: 50}}></View>
+            <Text style={[styles.title, {color: color.text}]}>
+              {translate('YourFeedback')}
+            </Text>
+            <Text style={[styles.subtitle, {color: color.textSubdued}]}>
+              {translate('FeedbackQuestion')}
+            </Text>
+            <View style={styles.ratingContainer}>
+              {Array.from({length: 5}, (_, index) => (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => handleRating(index + 1)}>
+                  <Ionicons
+                    name={index < rating ? 'star' : 'star-outline'}
+                    size={32}
+                    color={index < rating ? '#FFC727' : color.textSubdued}
+                  />
+                </TouchableOpacity>
+              ))}
+            </View>
+            <TextInput
+              style={[
+                styles.feedbackInput,
+                {backgroundColor: color.white, color: color.text, height: 150},
+              ]}
+              placeholder={translate('FeedbackInput')}
+              placeholderTextColor={color.textSubdued}
+              multiline
+              value={feedback}
+              onChangeText={setFeedback}
+            />
             <TouchableOpacity
-              key={index}
-              onPress={() => handleRating(index + 1)}>
-              <Ionicons
-                name={index < rating ? 'star' : 'star-outline'}
-                size={32}
-                color={index < rating ? '#FFD700' : '#ccc'}
-              />
+              style={styles.submitButton}
+              onPress={handleSubmit}>
+              <Text style={styles.submitButtonText}>
+                {translate('SubmitFeedback')}
+              </Text>
             </TouchableOpacity>
-          ))}
+          </View>
         </View>
-        <TextInput
-          style={[
-            styles.feedbackInput,
-            {backgroundColor: color.white, color: color.text},
-          ]}
-          placeholder={translate('FeedbackInput')}
-          placeholderTextColor={color.textSubdued}
-          multiline
-          value={feedback}
-          onChangeText={setFeedback}
-        />
-        <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
-          <Text style={styles.submitButtonText}>
-            {translate('SubmitFeedback')}
-          </Text>
-        </TouchableOpacity>
       </ScrollView>
     </KeyboardAvoidingView>
   );
