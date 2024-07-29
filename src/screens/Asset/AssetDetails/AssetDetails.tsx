9f57f21e2a9139db419e74d78551534c599cfa4a
dthgiang <duongthanhgiang.0108@gmail.com>
2024-07-29 21:27:53 +0700

    update

--- src/screens/Asset/AssetDetails/AssetDetails.tsx
+++ src/screens/Asset/AssetDetails/AssetDetails.tsx
@@ -31,6 +31,7 @@ import {Feather} from '@expo/vector-icons';
 import {ScrollView} from 'react-native-gesture-handler';
 import {COLORS} from 'src/constants';
 import {Toast} from 'react-native-toast-notifications';
+import styles from './styles';
 
 const AssetDetailScreen = ({route, navigation}: AssetDetailScreenProps) => {
   const asset = useSelector(selectSelectedAsset);
@@ -171,28 +172,10 @@ const AssetDetailScreen = ({route, navigation}: AssetDetailScreenProps) => {
           <TouchableOpacity onPress={() => navigation.goBack()}>
             <Icon name="arrow-back" size={30} color={color.text} />
           </TouchableOpacity>
-          <Text style={[styles.title, {color: color.text}]}>
-            {translate('Asset Detail')}
-          </Text>
+          <Text style={[styles.title, {color: color.text}]}>{asset?.name}</Text>
 
-          <TouchableOpacity
-            onPress={handleEditPress}
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              backgroundColor: '#00adf5',
-              padding: 15,
-              borderRadius: 30,
-              paddingHorizontal: 20,
-              shadowColor: '#00adf5',
-              shadowOffset: {width: 0, height: 4},
-              shadowOpacity: 0.3,
-              shadowRadius: 2,
-            }}>
-            <Feather name="edit" size={20} color="white" />
-            <Text style={{marginLeft: 10, fontWeight: '700', color: 'white'}}>
-              {translate('Edit')}
-            </Text>
+          <TouchableOpacity onPress={handleEditPress}>
+            <Feather name="edit" size={23} color={color.icon} />
           </TouchableOpacity>
         </View>
         <ScrollView contentContainerStyle={styles.scrollView}>
@@ -347,135 +330,5 @@ const AssetDetailScreen = ({route, navigation}: AssetDetailScreenProps) => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-  },
-  closeButton: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    zIndex: 1,
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 16,
-    justifyContent: 'space-between',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginLeft: 30,
-  },
-  imageContainer: {
-    alignItems: 'center',
-    marginVertical: 16,
-  },
-  assetImage: {
-    width: 300,
-    height: 300,
-    borderRadius: 8,
-  },
-  cameraIconContainer: {
-    position: 'absolute',
-    bottom: 10,
-    right: 10,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    borderRadius: 20,
-    padding: 8,
-  },
-  cameraIcon: {
-    color: '#fff',
-  },
-  noImageContainer: {
-    width: 200,
-    height: 200,
-    backgroundColor: '#f0f0f0',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 8,
-  },
-  noImageText: {
-    color: '#a0a0a0',
-    fontSize: 16,
-  },
-  assetInfo: {
-    flex: 1,
-    padding: 16,
-    borderRadius: 8,
-    marginHorizontal: 16,
-    elevation: 3,
-  },
-  assetDetailLabel: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginVertical: 8,
-  },
-  assetDetailText: {
-    fontSize: 16,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    padding: 8,
-    fontSize: 16,
-  },
-  multilineInput: {
-    height: 80,
-    textAlignVertical: 'top',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 14,
-    marginBottom: 20,
-    marginTop: 10,
-  },
-  button: {
-    flex: 1,
-    padding: 12,
-    borderRadius: 10,
-    marginHorizontal: 4,
-    alignItems: 'center',
-    backgroundColor: COLORS.Rhino,
-    paddingVertical: 15,
-  },
-  saveButton: {
-    backgroundColor: '#00adf5',
-  },
-  cancelButton: {
-    backgroundColor: '#f44336',
-  },
-  editButton: {
-    backgroundColor: '#2196f3',
-  },
-  deleteButton: {
-    backgroundColor: '#f44336',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  modal: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalContent: {
-    backgroundColor: '#fff',
-    padding: 16,
-    borderRadius: 8,
-    alignItems: 'center',
-  },
-  modalImage: {
-    width: 400,
-    height: 500,
-    borderRadius: 8,
-  },
-});
 
 export default AssetDetailScreen;
---
+++ src/screens/Asset/AssetDetails/styles.ts
@@ -0,0 +1,137 @@
+import {Dimensions, StyleSheet} from 'react-native';
+import {COLORS} from 'src/constants';
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  scrollView: {
+    flex: 1,
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    zIndex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  imageContainer: {
+    alignItems: 'center',
+    marginVertical: 16,
+  },
+  assetImage: {
+    width: '100%',
+    height: 300,
+    borderRadius: 8,
+  },
+  cameraIconContainer: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: 20,
+    padding: 8,
+  },
+  cameraIcon: {
+    color: '#fff',
+  },
+  noImageContainer: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#f0f0f0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  noImageText: {
+    color: '#a0a0a0',
+    fontSize: 16,
+  },
+  assetInfo: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    marginHorizontal: 16,
+    elevation: 3,
+  },
+  assetDetailLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginVertical: 8,
+  },
+  assetDetailText: {
+    fontSize: 16,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 8,
+    fontSize: 16,
+  },
+  multilineInput: {
+    height: 80,
+    textAlignVertical: 'top',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 14,
+    marginBottom: 20,
+    marginTop: 10,
+  },
+  button: {
+    flex: 1,
+    padding: 12,
+    borderRadius: 10,
+    marginHorizontal: 4,
+    alignItems: 'center',
+    backgroundColor: COLORS.Rhino,
+    paddingVertical: 15,
+  },
+  saveButton: {
+    backgroundColor: '#00adf5',
+  },
+  cancelButton: {
+    backgroundColor: '#f44336',
+  },
+  editButton: {
+    backgroundColor: '#2196f3',
+  },
+  deleteButton: {
+    backgroundColor: '#f44336',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  modalImage: {
+    width: 400,
+    height: 500,
+    borderRadius: 8,
+  },
+});
+export default styles;
