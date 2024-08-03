c82a300adbcd3e389947dab8627bfa62adb52c78
thuhien2105 <73421264+thuhien2105@users.noreply.github.com>
2024-08-03 16:46:24 +0700

    Revert "Update sth"
    This reverts commit cf95ed589059a631dd6f8e6ac1beef11db1b6c25.

--- src/screens/Calendar/EventDetails/EventDetails.tsx
+++ src/screens/Calendar/EventDetails/EventDetails.tsx
@@ -130,7 +130,18 @@ const EventDetailsScreen = ({route, navigation}: EventDetailsScreenProps) => {
                   event.start_timezone,
                   event.end_timezone,
                 );
-
+<<<<<<< Updated upstream
+                dispatch(
+                  deleteEventOnly({
+                    id_calendar: event.id_calendar,
+                    time_start: event.time_start,
+                  }),
+                );
+                Toast.show(translate('Event has been deleted successfully'), {
+                  type: 'success',
+                });
+                navigation.goBack();
+=======
                 if (data) {
                   dispatch(deleteEventOnly());
                   Toast.show(translate('Event has been deleted successfully'), {
@@ -140,6 +151,7 @@ const EventDetailsScreen = ({route, navigation}: EventDetailsScreenProps) => {
                 } else {
                   console.log('hi');
                 }
+>>>>>>> Stashed changes
               } catch (error) {
                 console.error('Error deleting event:', error);
                 Toast.show(
